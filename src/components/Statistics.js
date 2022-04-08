693b7faec043cca5b120e0d5e3d6063e83fd57fc
Marika Stenberg <stenberg.marika@outlook.com>
2022-04-08 15:08:31 +0300

    valmis

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,73 +1,40 @@
-import { divide } from 'lodash';
+import _ from 'lodash';
+import Button from '@material-ui/core/Button';
 import React, {useState, useEffect} from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 
 export default function Statistics () {
 
 
-  const [trainings, setTrainigs] = useState([]);
-  const [train, setTrain] = useState({name: '', duration: ''});
-  const [trains, setTrains] = useState([]);
+  const [trainings, setTrainings] = useState([{duration: '', name: ''}]);
 
   useEffect(() => fetchData(), []);
 
  const  fetchData = () => {
      fetch('https://customerrest.herokuapp.com/api/trainings')
      .then(response => response.json())
-     .then(data => setTrainigs(data.content))
+     .then(data => setTrainings(data.content))
      .catch(err => console.error(err))
-      divide();
-      console.log(trainings)
  }
 
-const divide = () => {
-  trainings.map((exercise) => 
-  setTrain({name: exercise.activity, duration: exercise.duration}),
-  setTrains([...trains, train])
-  );
-  console.log(trains)
-  console.log(trainings)
-}
+  const sum = () => {
+    console.log(trainings);
+    var ggroup = trainings;
+    _.find(ggroup, _.matchesProperty('Gym training'));
+    console.log(ggroup);
+    _.sumBy(trainings, 'activity');
+} 
 
-const data = [
-  {
-    name: 'Page A',
-    duration: 4000,
-  },
-  {
-    name: 'Page B',
-    duration: 3000,
-  },
-  {
-    name: 'Page C',
-    duration: 2000,
-  },
-  {
-    name: 'Page D',
-    duration: 2780,
-  },
-  {
-    name: 'Page E',
-    duration: 1890,
-  },
-  {
-    name: 'Page F',
-    duration: 2390,
-  },
-  {
-    name: 'Page G',
-    duration: 3490,
-  },
-];
-       
     return (
-      <BarChart width={730} height={250} data={trains}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+      <div>
+        <BarChart width={730} height={250} data={trainings}>
+          <XAxis dataKey="activity"/>
           <YAxis />
           <Tooltip />
           <Legend />
           <Bar dataKey="duration" fill="#8884d8" />
       </BarChart>
+      <Button onClick={() => sum()}>Show in groups</Button>
+      </div>
     );
 }
